39464d7ed34c2e58e95e41bf7fafb08c65c5f542
HARSHAN1712 <harshankaw@gmail.com>
2025-01-29 16:33:36 +0530

    portfolio

---
+++ obj.js
@@ -0,0 +1,10 @@
+let data=42;
+let dataCopy=data;
+dataCopy=49
+console.log(Object.data);
+let array=[2,4,5,6,7,8,9]
+let arrCopy=[...array,...arr2]
+//console.log(array);
+console.log(arrcopy);
+let user={name:"user1",age:"20"}
+let address={city:"CBE",country:"India"}
